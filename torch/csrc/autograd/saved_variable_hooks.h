de680493537a6737ef59888e59ac6b55dbd1c21b
Victor Quach <quach@fb.com>
2021-07-01 10:17:50 -0400

    Register Saved Tensors hooks
    Summary:
    
    Test Plan:
    
    Reviewers:
    
    Subscribers:
    
    Tasks:
    
    Tags:
    
    ghstack-source-id: 6e5ada3a3e6b8526ac5b368dc76917445127522a
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/60663

---
+++ torch/csrc/autograd/saved_variable_hooks.h
@@ -0,0 +1,13 @@
+#pragma once
+
+#include <torch/csrc/WindowsTorchApiMacro.h>
+#include <ATen/ATen.h>
+
+namespace torch { namespace autograd {
+
+struct TORCH_API SavedVariableHooks {
+  virtual PyObject* call_pack_hook(at::Tensor &tensor) = 0;
+  virtual at::Tensor call_unpack_hook(PyObject* obj) = 0 ;
+};
+
+}}
